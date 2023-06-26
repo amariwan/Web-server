0891aef89bd8604997c9acefad5ff9837cde6207
Administrator <Administrator@GSSDWS23>
2023-06-26 14:41:05 +0200

    Projektdateien hinzufügen.

---
+++ Web-server/Program.cs
@@ -0,0 +1,88 @@
+using System.Net.WebSockets;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World! new");
+app.MapGet("/test", () => "Hello World Aland!");
+
+app.UseWebSockets();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+var webSocketOptions = new WebSocketOptions
+{
+    KeepAliveInterval = TimeSpan.FromMinutes(2)
+};
+
+app.UseWebSockets(webSocketOptions);
+
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/ws")
+    {
+        if (context.WebSockets.IsWebSocketRequest)
+        {
+            var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+            await HandleWebSocket(webSocket);
+        }
+        else
+        {
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+        }
+    }
+    else
+    {
+        await next();
+    }
+});
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+async Task HandleWebSocket(WebSocket webSocket)
+{
+    // WebSocket handling logic here
+    // For example, you can use a while loop to continuously receive and send messages
+    var buffer = new byte[1024];
+    WebSocketReceiveResult result;
+
+    do
+    {
+        result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+        if (result.MessageType == WebSocketMessageType.Text)
+        {
+            var receivedMessage = Encoding.UTF8.GetString(buffer, 0, result.Count);
+            // Process the received message
+
+            var responseMessage = $"Received: {receivedMessage}";
+            
+            var responseBytes = Encoding.UTF8.GetBytes(responseMessage);
+            await webSocket.SendAsync(new ArraySegment<byte>(responseBytes), WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+        else if (result.MessageType == WebSocketMessageType.Close)
+        {
+            // WebSocket close message received
+            break;
+        }
+    } while (!result.CloseStatus.HasValue);
+
+    await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+}
