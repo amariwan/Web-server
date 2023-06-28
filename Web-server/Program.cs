1fdb12e9f290d9876b39c07ca668bc03db1fe29b
Aland Mariwan <kontakt@aland-mariwan.de>
2023-06-28 18:40:23 +0200

    new

--- Web-server/Program.cs
+++ Web-server/Program.cs
@@ -7,13 +7,56 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// Configure CORS
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
 
 var app = builder.Build();
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthorization();
+app.UseCors();
 
-app.MapGet("/", () => "Hello World! new");
-app.MapGet("/test", () => "Hello World Aland!");
+app.MapGet("/", () =>
+{
+    Console.WriteLine("Hello, fearless explorer!");
+    return "Welcome to the magical world of code!";
+});
 
-app.UseWebSockets();
+app.MapGet("/{name:alpha}", (string name) =>
+{
+    if (name.ToLower() == "openai")
+    {
+        Console.WriteLine($"Hello, esteemed {name}! You're a wizard of AI!");
+        return $"Hello, esteemed {name}! You're a wizard of AI!";
+    }
+    else
+    {
+        Console.WriteLine($"Hello, {name}! Did you bring your sense of humor today?");
+        return $"Hello, {name}! Did you bring your sense of humor today?";
+    }
+});
+
+app.MapGet("/hello/{name:alpha}", (string name) =>
+{
+    Console.WriteLine($"Hello, {name}! Let's have a dance-off with the robots!");
+    return $"Hello, {name}! Let's have a dance-off with the robots!";
+});
+
+app.MapPost("/test/", () =>
+{
+    Console.WriteLine("Well, it was fun");
+});
+
+
 
 if (app.Environment.IsDevelopment())
 {
@@ -30,7 +73,8 @@ app.UseWebSockets(webSocketOptions);
 
 app.Use(async (context, next) =>
 {
-    if (context.Request.Path == "/ws")
+    Console.WriteLine($"Path: {context.Request.Path}");
+    if (context.Request.Path == "/wss")
     {
         if (context.WebSockets.IsWebSocketRequest)
         {
@@ -48,17 +92,10 @@ app.Use(async (context, next) =>
     }
 });
 
-app.UseHttpsRedirection();
-app.UseAuthorization();
 
 app.MapControllers();
-
-app.Run();
-
 async Task HandleWebSocket(WebSocket webSocket)
 {
-    // WebSocket handling logic here
-    // For example, you can use a while loop to continuously receive and send messages
     var buffer = new byte[1024];
     WebSocketReceiveResult result;
 
@@ -69,19 +106,18 @@ async Task HandleWebSocket(WebSocket webSocket)
         if (result.MessageType == WebSocketMessageType.Text)
         {
             var receivedMessage = Encoding.UTF8.GetString(buffer, 0, result.Count);
-            // Process the received message
-
             var responseMessage = $"Received: {receivedMessage}";
-            
+
             var responseBytes = Encoding.UTF8.GetBytes(responseMessage);
             await webSocket.SendAsync(new ArraySegment<byte>(responseBytes), WebSocketMessageType.Text, true, CancellationToken.None);
         }
         else if (result.MessageType == WebSocketMessageType.Close)
         {
-            // WebSocket close message received
             break;
         }
     } while (!result.CloseStatus.HasValue);
 
     await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
 }
+
+await app.RunAsync();
