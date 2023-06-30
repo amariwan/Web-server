3d7a8ad23ff30db294f89eb8ee77bd0fb14b7f83
Aland Mariwan <dev@aland-mariwan.de>
2023-06-30 09:29:32 +0200

    Update Program.cs

--- Web-server/Program.cs
+++ Web-server/Program.cs
@@ -1,5 +1,15 @@
+using System;
+using System.Collections.Concurrent;
+using System.Net.Http;
 using System.Net.WebSockets;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json.Linq;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,10 +34,38 @@ app.UseHttpsRedirection();
 app.UseRouting();
 app.UseAuthorization();
 app.UseCors();
+app.MapControllers();
+// Store WebSocket connections
+var webSockets = new ConcurrentDictionary<string, WebSocket>();
+
+
+app.Use(async (context, next) =>
+{
+    var currentEndpoint = context.GetEndpoint();
+
+    if (currentEndpoint is null)
+    {
+        await next(context);
+        return;
+    }
+
+    Console.WriteLine($"Endpoint: {currentEndpoint.DisplayName}");
+
+    if (currentEndpoint is RouteEndpoint routeEndpoint)
+    {
+        Console.WriteLine($"  - Route Pattern: {routeEndpoint.RoutePattern}");
+    }
+
+    foreach (var endpointMetadata in currentEndpoint.Metadata)
+    {
+        Console.WriteLine($"  - Metadata: {endpointMetadata}");
+    }
+
+    await next(context);
+});
 
 app.MapGet("/", () =>
 {
-    Console.WriteLine("Hello, fearless explorer!");
     return "Welcome to the magical world of code!";
 });
 
@@ -79,7 +117,9 @@ app.Use(async (context, next) =>
         if (context.WebSockets.IsWebSocketRequest)
         {
             var webSocket = await context.WebSockets.AcceptWebSocketAsync();
-            await HandleWebSocket(webSocket);
+            var clientId = Guid.NewGuid().ToString();
+            webSockets.TryAdd(clientId, webSocket);
+            await HandleWebSocket(clientId);
         }
         else
         {
@@ -91,11 +131,10 @@ app.Use(async (context, next) =>
         await next();
     }
 });
-
-
-app.MapControllers();
-async Task HandleWebSocket(WebSocket webSocket)
+async Task HandleWebSocket(string clientId)
 {
+    var webSocket = webSockets[clientId];
+
     var buffer = new byte[1024];
     WebSocketReceiveResult result;
 
@@ -106,18 +145,71 @@ async Task HandleWebSocket(WebSocket webSocket)
         if (result.MessageType == WebSocketMessageType.Text)
         {
             var receivedMessage = Encoding.UTF8.GetString(buffer, 0, result.Count);
-            var responseMessage = $"Received: {receivedMessage}";
+            Console.WriteLine($"Received: {receivedMessage}");
 
-            var responseBytes = Encoding.UTF8.GetBytes(responseMessage);
-            await webSocket.SendAsync(new ArraySegment<byte>(responseBytes), WebSocketMessageType.Text, true, CancellationToken.None);
+            // Process the received message
+            var httpClient = new HttpClient();
+            var encodedMsg = Uri.EscapeDataString(receivedMessage);
+            var apiKey = "sk-***************************************";
+            var apiUrl = "https://api.openai.com/v1/chat/completions";
+
+            var data = new
+            {
+                model = "gpt-3.5-turbo",
+                messages = new[]
+                {
+                    new { role = "system", content = "You are a helpful assistant." },
+                    new { role = "user", content = receivedMessage }
+                }
+            };
+
+            httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+
+            try
+            {
+                var response = await httpClient.PostAsync(apiUrl, new StringContent(Newtonsoft.Json.JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json"));
+                var responseBody = await response.Content.ReadAsStringAsync();
+
+                dynamic resultJson = Newtonsoft.Json.JsonConvert.DeserializeObject(responseBody);
+
+                // Get the assistant's reply
+                var reply = resultJson.choices[0].message.content.ToString();
+                Console.WriteLine("Assistant: " + reply);
+
+                var responseBytes = Encoding.UTF8.GetBytes(reply);
+
+                // Send the message only to the requesting client
+                await webSocket.SendAsync(new ArraySegment<byte>(responseBytes), WebSocketMessageType.Text, true, CancellationToken.None);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error sending request: {ex.Message}");
+            }
         }
         else if (result.MessageType == WebSocketMessageType.Close)
         {
+            webSockets.TryRemove(clientId, out _);
             break;
         }
     } while (!result.CloseStatus.HasValue);
 
     await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
 }
+
+
+// Start reading from the console and send input to WebSocket clients
+_ = Task.Run(async () =>
+{
+    while (true)
+    {
+        var input = Console.ReadLine();
+        var responseBytes = Encoding.UTF8.GetBytes(input);
+
+        foreach (var socket in webSockets.Values)
+        {
+            await socket.SendAsync(new ArraySegment<byte>(responseBytes), WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+    }
+});
 
 await app.RunAsync();
